d7840654a2ca048ea910ba3abba8c2cbea17631d
AvniJ237 <avnijain2307@gmail.com>
2026-06-18 10:41:37 +0530

    Add program to check divisibility of numbers

--- number.c
+++ number.c
@@ -1,1 +1,24 @@
-
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{int choice,num,div;
+printf("Enter 1 to find whether a number is divisible by a certain number and 0 to exit");
+scanf("%d",&choice);
+switch(choice)
+   { case 1:printf("enter the number and the number to checks its divisiblity with");
+        scanf("%d %d",&num, &div);
+        if(num>=div)
+          {if(num%div==0)
+            {printf("divisible");
+          }
+           else
+             {printf("not divisible");}
+          }
+        else
+        {printf("pls enter correct numbers");}
+         break;
+    case 0:printf("exiting...");
+          break;
+   }
+  getch();
+}
