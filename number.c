1b628789bd08e02098a34f4e4bd019309720e212
AvniJ237 <avnijain2307@gmail.com>
2026-06-19 08:24:01 +0530

    Enhance number.c with reverse functionality and validation
    Updated the main function to include reverse number functionality and improved input validation for divisibility check.

--- number.c
+++ number.c
@@ -1,24 +1,33 @@
 #include<stdio.h>
-#include<conio.h>
-void main ()
-{int choice,num,div;
-printf("Enter 1 to find whether a number is divisible by a certain number and 0 to exit");
+int main ()
+{int choice,num,div,rev=0;
+printf("Enter 1 to find whether a number is divisible by a certain number,\n 2 to find reverse of the number\n and 0 to exit");
 scanf("%d",&choice);
 switch(choice)
    { case 1:printf("enter the number and the number to checks its divisiblity with");
         scanf("%d %d",&num, &div);
-        if(num>=div)
-          {if(num%div==0)
+        if(div!=0)
+        {if(num%div==0)
             {printf("divisible");
           }
            else
              {printf("not divisible");}
-          }
+        }
         else
-        {printf("pls enter correct numbers");}
+        {printf("Did you really type zero?");}
          break;
+    case 2:printf("enter the number ");
+           scanf("%d",&num);
+           do
+           {rev=rev*10+num%10;
+           num=num/10;}
+           while(num!=0);
+           printf("reverse is %d",rev);
+           break;
     case 0:printf("exiting...");
           break;
+    default:
+    printf("Invalid choice");
    }
-  getch();
+ return 0;
 }
