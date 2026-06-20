5b2185bfecf3b30e420cc00631d90cf151d0d3f8
AvniJ237 <avnijain2307@gmail.com>
2026-06-20 14:45:58 +0530

    Expand number program with new features
    Added functionality to check for factors, determine if a number is even or odd, and identify prime or composite numbers.

--- number.c
+++ number.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main ()
-{int choice,num,div,rev=0;
-printf("Enter 1 to find whether a number is divisible by a certain number,\n 2 to find reverse of the number\n and 0 to exit");
+{int choice,num,div,rev=0,i,count=0;
+printf("Enter 1 to find whether a number is divisible by a certain number,\n 2 to find reverse of the number\n 3 to find factors of a number and\n 4 for overview of number 0 to exit");
 scanf("%d",&choice);
 switch(choice)
    { case 1:printf("enter the number and the number to checks its divisiblity with");
@@ -24,10 +24,42 @@ switch(choice)
            while(num!=0);
            printf("reverse is %d",rev);
            break;
+    case 3:printf("enter the number ");
+           scanf("%d",&num);
+           for(i=1; i<=num; i++)
+           {if(num % i == 0)
+           { printf("%d, ", i);}}
+           break;
+    case 4:printf("enter the number ");
+           scanf("%d",&num);
+           if(num % 2 == 0)
+           { printf("Even Number\n");}
+           else
+           {printf("Odd Number\n");}
+           if(num > 0)
+           { printf("positive Number\n");}
+           else if(num==0)
+           {printf("its a zero!!!\n");}
+           else
+           {printf("negative Number\n");}
+           if(num<0)
+           {num=-num;}
+           for(i=1; i<=num; i++)
+           {if(num % i == 0)
+           { count++;}}
+           if (num==0)
+           {printf("zero isnt prime or composite\n");}
+           else if (count>2)
+           {printf("Composite Number\n");}
+           else if(count==1)
+           {printf("one isnt prime or composite\n");}
+           else 
+           {printf("Prime Number\n");}
+           break;
     case 0:printf("exiting...");
           break;
     default:
-    printf("Invalid choice");
+    printf("Intersting choice but nope...");
    }
  return 0;
 }
