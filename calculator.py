41ddb98c9250ed593f0d1567cb54fafbefe317aa
AvniJ237 <avnijain2307@gmail.com>
2026-06-23 10:01:22 +0530

    Implement basic calculator functionality

--- calculator.py
+++ calculator.py
@@ -1,1 +1,17 @@
+a = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+b = float(input("Enter second number: "))
+if operator == "+":
+    print("Result:", a + b)
+elif operator == "-":
+    print("Result:", a - b)
+elif operator == "*":
+    print("Result:", a * b)
+elif operator == "/":
+    if b!= 0:
+        print("Result:", a / b)
+    else:
+        print("cant divide with zero")
+else:
+    print("Invalid operator")
 
